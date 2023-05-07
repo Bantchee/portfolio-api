dd973416834434b8dd9d5e73bbdcc5ba5385555c
Jar'Zeno <jarzenojarrett@gmail.com>
2023-05-07 00:28:27 -0700

    Add permission attribute to User model

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+const { stringify } = require('jade/lib/utils');
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
@@ -6,6 +7,12 @@ const UserSchema = new Schema({
     username: { type: String, require: true, maxLength: 100 },
     email: { type: String, require: true },
     password: { type: String, require: true, maxLength: 100 },
+    permissions: {
+        type: stringify,
+        require: true,
+        enum: ["adim", "base"],
+        default: "base",
+    },
     creation_date: { type: Date, default: Date.now },
 });
 
